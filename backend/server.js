54a93ddbe9ad66f37719cdf3c29e8ad0b7896c8d
Prince <12prince345singh@gmail.com>
2025-07-21 12:23:01 +0530

    MongoDb database setup and CORS completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('FocusFlow API is running...');
